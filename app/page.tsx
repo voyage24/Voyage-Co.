a529576f307b6d832c2a0c53ae767763230a613a
voage24 <lhvindia@gmail.com>
2026-06-26 22:31:16 +0530

    Remove duplicate booking widget section near the footer on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/home/HeroSection";
 import PopularDestinations from "@/components/home/PopularDestinations";
 import SignatureExperiences from "@/components/home/SignatureExperiences";
-import ReservationSection from "@/components/home/ReservationSection";
 import PackagesPreview from "@/components/home/PackagesPreview";
 import TrustSection from "@/components/home/TrustSection";
 import { prisma } from "@/lib/prisma";
@@ -17,14 +16,11 @@ export default async function Home() {
     prisma.experience.findMany({ where: { published: true } }),
   ]);
 
-  const hotelCities = Array.from(new Set(hotels.map(h => h.city))).sort();
-
   return (
     <>
       <HeroSection hotels={hotels} cruises={cruises} trains={trains as any} packages={packages} experiences={experiences} />
       <PopularDestinations />
       <SignatureExperiences />
-      <ReservationSection hotelCities={hotelCities} />
       <PackagesPreview packages={packages} />
       <TrustSection />
     </>
